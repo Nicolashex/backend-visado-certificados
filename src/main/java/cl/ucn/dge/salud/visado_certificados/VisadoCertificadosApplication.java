34a6532fc9f2c9eaecd3808a72580b61cfa5429b
Nicolas Henriquez <nicolashex1@gmail.com>
2021-11-03 23:59:34 -0300

    first commit

---
+++ src/main/java/cl/ucn/dge/salud/visado_certificados/VisadoCertificadosApplication.java
@@ -0,0 +1,13 @@
+package cl.ucn.dge.salud.visado_certificados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisadoCertificadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisadoCertificadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ucn/dge/salud/visado_certificados/VisadoCertificadosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ucn.dge.salud.visado_certificados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisadoCertificadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
