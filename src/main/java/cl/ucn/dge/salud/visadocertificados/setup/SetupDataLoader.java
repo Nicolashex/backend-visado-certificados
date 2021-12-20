d5a14e1b4f1557588e2b314f61ee49498cc90897
Nicolas Henriquez <nicolashex1@gmail.com>
2021-12-20 20:51:34 -0300

    feat: corregir correo en blanco.

--- src/main/java/cl/ucn/dge/salud/visadocertificados/setup/SetupDataLoader.java
+++ src/main/java/cl/ucn/dge/salud/visadocertificados/setup/SetupDataLoader.java
@@ -59,7 +59,7 @@ public class SetupDataLoader implements
         crearMedico("medico3@correo.cl","3333333-2","contraseña3","Nicolas",
                 "Pereira","Farfan","+569231412522","Personal evaluador","Psicologo");
 
-        crearMedico(" ","6666666-2","contraseña3","Paula",
+        crearMedico("medico4@correo.cl","6666666-2","contraseña3","Paula",
                 "Daza","Rojas","+569231412522","Personal evaluador","Medico general");
 
         crearCarrera("Postgrado","General","UCN");
